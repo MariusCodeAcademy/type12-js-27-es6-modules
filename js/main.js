a7b847158337d39f9f4c1201d96c369a4dcc6fba
Marius <marius@example.com>
2022-08-12 11:13:48 +0300

    http

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ console.log('main.js');
 // import named exported function
 import { addNums, minusNums } from './modules/operations.js';
 // importuoti num1 ir num2 ir panaudoti funkcijos addNums skaiciavimui
+
 import { num11 as n1, num2 as n2, arr } from './modules/variables.js';
+// import * as kintai from './modules/variables.js';
+
 // importuoti numatytaji exporta
 import sayHiHere from './modules/sayHi.js';
 import calcAvg from './modules/avg.js';
 // import calcAvg from './modules/avg.js';
 import { displayAfterH1 } from './modules/htmlDisplay.js';
+import http from './modules/http.js';
 
 const rez1 = addNums(n1, n2);
 console.log('rez1 ===', rez1);
@@ -23,3 +27,6 @@ console.log('minusrez ===', minusrez);
 const avg1 = calcAvg(arr);
 console.log('avg1 ===', avg1.toFixed(2));
 displayAfterH1(avg1.toFixed(2));
+
+const users = await http('https://jsonplaceholder.typicode.com/dfsdfsdfsdf');
+console.log('users ===', users);
--- js/modules/http.js
+++ js/modules/http.js
@@ -1,1 +1,17 @@
 // parasyti ir exportuoti http funkcija kuri grazina fetch uzklausos resp.json() dali su async ir await
+
+async function http(url) {
+  try {
+    const resp = await fetch(url);
+    if (!resp.ok)
+      throw new Error(`fetch fail: ${resp.status} ${resp.statusText} trying to fetch ${resp.url}`);
+    const dataInJs = await resp.json();
+    console.log('http:', dataInJs);
+    return dataInJs;
+  } catch (error) {
+    console.warn('klaida http ', error.message);
+    console.warn('error.stack ', error.stack);
+  }
+}
+
+export default http;
